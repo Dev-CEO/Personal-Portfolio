bc7d6b31bb1258efadc84a8258a9c87297df71f4
Dev-CEO <samsoorsahilllc555@gmail.com>
2025-05-16 22:05:18 +0430

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var typed = new Typed(".multiple-text", {
+    strings : ["Front-End Developer", "Back-End Developer"],
+    typeSpeedy : 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+    showCursor: true,
+    cursorChar: "|",
+    contentType: 'text',
+});
+
+
+let btn = document.querySelector(".btn");
+btn.addEventListener("click", () => {
+
+})
